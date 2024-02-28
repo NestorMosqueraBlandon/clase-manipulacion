b91f9cb7c8a8be810e586e70da8c32b8fc103252
NestorMosqueraBmatch <129396602+NestorMosqueraBmatch@users.noreply.github.com>
2024-02-27 20:47:27 -0500

    chore: ejerciio 3

---
+++ ejercicio_martes.py
@@ -0,0 +1,17 @@
+"""
+Has sido contratado por una agencia gubernamental para analizar la calidad del agua en diferentes cuerpos de agua de una región. Te han proporcionado un conjunto de datos que contiene mediciones de diferentes parámetros de calidad del agua, pero el conjunto de datos tiene problemas como valores atípicos y formatos incorrectos. Tu tarea es limpiar los datos utilizando las siguientes técnicas:
+
+Identificar y manejar los valores atípicos:
+
+Carga el conjunto de datos y visualiza la distribución de los diferentes parámetros de calidad del agua.
+Utiliza métodos estadísticos o gráficos para identificar valores atípicos en los datos.
+Aplica una estrategia para manejar los valores atípicos, como eliminarlos o transformarlos.
+Manejar formatos incorrectos:
+
+Verifica si hay problemas de formato en las fechas u otros campos.
+Corrige los problemas de formato para asegurarte de que los datos sean coherentes y puedan ser analizados correctamente.
+Verificar la calidad de los datos limpios:
+
+Después de realizar las operaciones de limpieza, verifica la calidad de los datos resultantes mediante estadísticas descriptivas y visualizaciones.
+
+"""
